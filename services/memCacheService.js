7a00ee43b8c397bfb1b13d97555839445113e6f0
Oliver Liu <spaghettarian+git@gmail.com>
2018-04-18 21:05:08 -0700

    Added some debug message

--- services/memCacheService.js
+++ services/memCacheService.js
@@ -2,10 +2,13 @@ module.exports = function (memCache) {
   return {
     getByName: async (name) => {
       try {
+        console.log(`Getting ${name} from Redis`)
         const res = await memCache.get(name)
         if (!res) {
+          console.log(`Could not find ${name} in Redis`)
           return null
         } else {
+          console.log(`Found ${name} in Redis`)
           return JSON.parse(res)
         }
       } catch (err) {
@@ -15,6 +18,7 @@ module.exports = function (memCache) {
     },
     insertByName: async (name, ex, data) => {
       try {
+        console.log(`Inserting ${name} into Redis`)
         await memCache.setex(name, ex, data)
       } catch (err) {
         console.error(err)
